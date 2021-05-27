fe82944c92301150ef20bf131d9ae7eb0d9b373d
MartinPucan <pucan.m@seznam.cz>
2021-05-27 03:13:37 +0200

    removed import js files

--- src/js/math.js
+++ src/js/math.js
@@ -1,6 +1,11 @@
-const sum = (a, b) => a + b
-const subtract = (a, b) => a - b
-const divide = (a, b) => a / b
-const multiplication = (a, b) => a * b
+export function sum(a, b) {
+    return a + b
+}
 
-module.exports = {sum, subtract, divide, multiplication}
+const subtract = (a, b) => a - b
+
+export default subtract();
+// const divide = (a, b) => a / b
+// const multiplication = (a, b) => a * b
+
+// module.exports = { subtract, divide, multiplication }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,5 @@
 import { test, expect } from './functions/index'
-const {sum, subtract} = require('../src/js/math')
-// const {divide, multiplication} = require('../src/js/math')
+import { sum } from '../src/js/math'
 
 let result, expected
 
@@ -8,12 +7,12 @@ result = sum(3, 7)
 expected = 10
 // expect(result).toBe(expected)
 
-result = subtract(7, 3)
-expected = 4
+// result = subtract(7, 3)
+// expected = 4
 // expect(result).toBe(expected)
 
 test('test substract', () => {
-    const result = subtract(18, 10)
+    const result = sum(18, 10)
     const expected = 8
     expect(result).toBe(expected)
 })
